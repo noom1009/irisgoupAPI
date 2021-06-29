503e9484f5e5b439c0c036635106f410b3ef113c
irisgroup <noom1009@gmail.com>
2021-06-29 14:41:47 +0700

    commit

---
+++ controllers/finDownControllers.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const router = express.Router();
+const axios = require("axios");
+const cors = require("cors");
+const app = express();
+const log4js = require("log4js");
+const log = log4js.getLogger();
+const logger = require("morgan");
+const session = require("express-session");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const md5 = require("md5");
+const nodemailer = require("nodemailer");
+const db = require("../models/index");
+const sequelize = db.sequelize;
+const env = require("../config/env");
+const setting = require("../config/setting");
+const saltRounds = env.saltRounds;
+const lang = require("../lang/langApp");
+const con = require("../config/mysql");
+let tsData = Date.now();
+const moment = require("moment");
+const momentTZ = require("moment-timezone");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+const now = new Date();
+const dateString = moment(now).tz("Asia/Bangkok").format("D/M/Y");
+const dateFormat = moment(now).tz("Asia/Bangkok").format("YYYY-MM-DD hh:mm:ss");
+
+exports.getPageController = async (req, res, next) => {
+  res.json("Fin Down");
+};
+
+exports.finDownController = (req, res, next) => {};
---
+++ routers/authen/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const router = express.Router();
+const path = require("path");
+const session = require("express-session");
+const md5 = require("md5");
+const jwt = require("jsonwebtoken");
+const axios = require("axios");
+const crypto = require("crypto");
+const env = require("../../config/env");
+const setting = require("../../config/setting");
+const lang = require("../../lang/langApp");
+const authenControllers = require("../../controllers/authenControllers");
+
+router.get(
+  "/",
+  authenControllers.getPageController,
+  function (req, res, next) {}
+);
+
+module.exports = router;
